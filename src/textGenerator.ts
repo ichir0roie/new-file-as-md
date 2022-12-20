1e8cc20c24d92c17a6b4751b0f5f66f1379c9703
ichir0roieHome <ichir0roie.com>
2022-12-20 21:30:11 +0900

    add settings

--- src/textGenerator.ts
+++ src/textGenerator.ts
@@ -2,62 +2,64 @@ import * as cmn from './common';
 
 import * as vcu from "./vscodeCustomUtil";
 
-export class TextGenerator{
-    text:string='';
-    title:string='';
-    extension:string='';
-    dateString:cmn.DateString|undefined;
-    modeNameIsDate:boolean;
+export class TextGenerator {
+    text: string = '';
+    title: string = '';
+    extension: string = '';
+    dateString: cmn.DateString | undefined;
+    modeNameIsDate: boolean;
 
     constructor(
-        title:string,
-        extension:string,
-        dateString:cmn.DateString|undefined,
-        modeNameIsDate:boolean){
-        this.title=title;
-        this.extension=extension;
-        this.dateString=dateString;
-        this.modeNameIsDate=modeNameIsDate;
+        title: string,
+        extension: string,
+        dateString: cmn.DateString | undefined,
+        modeNameIsDate: boolean) {
+        this.title = title;
+        this.extension = extension;
+        this.dateString = dateString;
+        this.modeNameIsDate = modeNameIsDate;
     }
 
-    generate(){
-        
-        switch(this.extension){
+    generate() {
+
+        switch (this.extension) {
             case 'md':
                 this.markdown();
         }
 
         return this.text;
     }
-    
+
     markdown() {
-        const insertCreate=vcu.getConfiguration("newfileasmd.insertCreate");
-        const insertAuthor=vcu.getConfiguration("newfileasmd.insertAuthor");
-        const author=vcu.getConfiguration("newfileasmd.author");
-        
-        let text='';
-        
-        if(this.dateString!==undefined&&insertCreate){
-            text+="create : "+this.dateString.formatSlash()+"\n";
-        }
-        if(author!==undefined &&author.length>0&&insertAuthor){
-            text+="author : "+author+"\n";
-        }
+        const insertCreate: boolean = vcu.getConfiguration("newfileasmd.insertCreate");
+        const insertAuthor: boolean = vcu.getConfiguration("newfileasmd.insertAuthor");
+        const authorPrefix: string = vcu.getConfiguration("newfileasmd.authorPrefix");
+        const createPrefix: string = vcu.getConfiguration("newfileasmd.createPrefix");
+        const author = vcu.getConfiguration("newfileasmd.author");
 
-        if (this.title!==""&&!this.modeNameIsDate){
-            text+='\n# '+this.title+'\n\n';
+        let text = '';
+
+        if (this.dateString !== undefined && insertCreate) {
+            text += createPrefix + this.dateString.formatSlash() + "\n";
+        }
+        if (author !== undefined && author.length > 0 && insertAuthor) {
+            text += authorPrefix + author + "\n";
         }
 
-        text+=this.line20();
+        if (this.title !== "" && !this.modeNameIsDate) {
+            text += '\n# ' + this.title + '\n\n';
+        }
 
-        this.text=text;
+        text += this.line20();
+
+        this.text = text;
     }
 
 
-    line20(){
-        let text='';
-        for(let i=0;i<20;i++){
-            text+='\n';
+    line20() {
+        let text = '';
+        for (let i = 0; i < 20; i++) {
+            text += '\n';
         }
         return text;
     }
